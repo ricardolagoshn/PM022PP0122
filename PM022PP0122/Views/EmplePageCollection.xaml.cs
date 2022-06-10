0a67c27461b5dc0d92b2714fddab78d18c6a36dc
Ricardo.lagos <ricardolagoshn>
2022-06-09 20:05:40 -0600

    Cambios 09/06/2022

---
+++ PM022PP0122/Views/EmplePageCollection.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM022PP0122.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmplePageCollection : ContentPage
+    {
+        public EmplePageCollection()
+        {
+            InitializeComponent();
+        }
+
+        private void ListEmple_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
